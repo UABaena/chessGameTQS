24ba5c88d8e63f14cd13b70f4103cbbf2d6db326
Sergio <sergio.manchon@e-campus.uab.cat>
2019-10-31 12:22:11 +0100

    [TEST] board : Comprueba que los posibles movimientos se han realizado correctamente.

--- test/chessGameTQS/testBoard.java
+++ test/chessGameTQS/testBoard.java
@@ -144,18 +144,68 @@ class testBoard {
 	@Test
 	void testMovePiece() {
 		
-		//Rey, movimiento posible
+		//Inicializamos el tablero vacío para poder testear el movimiento de las piezas libremente
+		
+		for (int i = 0; i < board.NUM_ROWS; i++) {
+			
+			for (int j = 0; j < board.NUM_COLS; j++){
+				
+				board.setSquare(new Square(null,i,j));
+				
+				
+			}			
+		}
+		
+		//Ejecutando con el rey un mivimiento posible
 		Square s1 = new Square(new King(board.PLAYER_1), 0, 0);
 		Square s2 = new Square(null, 1, 1);
 		boolean result = board.movePiece(s1, s2);
 		assertTrue(result);
 		
+		//Comprovar que la posición final contiene el rey
+		Square expected = new Square(new King(board.PLAYER_1), 1, 1);
+		assertEquals(expected, s2);
 		
-		//Caballo movimiento imposible
+		//Comprovar que la posición inicial de rey está vacía
+		Square expectedNull = new Square(null, 0, 0);
+		assertEquals(expectedNull, s1);
+		
+		
+		/* Caballo movimiento imposible */
+		
 		s1 = new Square(new Knight(board.PLAYER_2), 4, 4);
 		s2 = new Square(null, 5, 5);
 		result = board.movePiece(s1, s2);
 		assertFalse(result);
+		
+		//Comprovar que la posición inicial contiene aún la reina
+		Square expected = new Square(new Knight(board.PLAYER_2), 5, 5);
+		assertEquals(expected, s1);
+		
+		//Comprovar que la posición final sigue vacía
+		Square expectedNull = new Square(null, 5, 5);
+		assertEquals(expectedNull, s2);
+		
+		
+		/*Reina movimiento fuera*/
+		
+		s1 = new Square(new Queen(board.PLAYER_1), 4, 0);
+		s2 = new Square(null, 4,8);
+		
+		result = board.movePiece(s1, s2);
+		assertFalse(result);
+		
+		//Comprovar que la posición inicial contiene aún la reina
+		
+		Square expected = new Square(new Queen(board.PLAYER_2), 4, 0);
+		assertEquals(expected, s1);
+		
+		//Comprovar que la posición final sigue vacía
+		
+		Square expectedNull = new Square(null, 4, 8);
+		assertEquals(expectedNull, s2);
+		
+		
 		
 		
 		

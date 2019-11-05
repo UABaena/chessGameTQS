1f4bda0d47d869878ec9fc8a21c347938a907241
Alex Velasco <131alexx@gmail.com>
2019-11-05 19:10:53 +0100

    [FEAT] pieces: implement getPossibleMoves on rook, bishop & pawn + update README.md

--- src/chessGameTQS/Rook.java
+++ src/chessGameTQS/Rook.java
@@ -1,5 +1,6 @@
 package chessGameTQS;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -7,9 +8,118 @@ public class Rook extends Piece {
 		this.name = "Torre";
 		this.player = player;}
 
-	public List<Square> getPossibleMoves(Board b, int row, int column) {
+	public List<Square> getPossibleMoves(Board b, int r, int c) {
 
-		return null;
+		int row = r, col = c;
+		List<Square> list = new ArrayList<Square>();
+
+
+		// Probar Arriba
+		row--;
+		boolean continueAdding = true;
+		
+		while (row >= 0 && continueAdding) {
+			Square s = b.getSquare(row, col);
+
+			Piece p = s.getPiece();
+
+			continueAdding = false;
+
+			if (p == null) { // Casilla vacia
+				list.add(s);
+				continueAdding = true;
+			} else {
+
+				if (p.getPlayer() != this.player) { // Otro jugador
+					list.add(s);
+				}
+			}
+			row--;
+
+		}
+		
+		// Probar Abajo
+		row = r;
+		col = c;
+		
+		row++;
+		continueAdding = true;
+		while (row < b.NUM_ROWS && continueAdding) {
+			Square s = b.getSquare(row, col);
+
+			Piece p = s.getPiece();
+
+			continueAdding = false;
+
+			if (p == null) { // Casilla vacia
+				list.add(s);
+				continueAdding = true;
+			} else {
+
+				if (p.getPlayer() != this.player) { // Otro jugador
+					list.add(s);
+				}
+			}
+			row++;
+
+		}
+		
+		
+		// Probar Izquierda
+		row = r;
+		col = c;
+		
+		col--;
+		continueAdding = true;
+		while (col >= 0 && continueAdding) {
+			Square s = b.getSquare(row, col);
+
+			Piece p = s.getPiece();
+
+			continueAdding = false;
+
+			if (p == null) { // Casilla vacia
+				list.add(s);
+				continueAdding = true;
+			} else {
+
+				if (p.getPlayer() != this.player) { // Otro jugador
+					list.add(s);
+				}
+			}
+			col--;
+
+		}
+		
+		// Probar Derecha
+		row = r;
+		col = c;
+		
+		col++;
+		continueAdding = true;
+		while (row < b.NUM_COLS && continueAdding) {
+			Square s = b.getSquare(row, col);
+
+			Piece p = s.getPiece();
+
+			continueAdding = false;
+
+			if (p == null) { // Casilla vacia
+				list.add(s);
+				continueAdding = true;
+			} else {
+
+				if (p.getPlayer() != this.player) { // Otro jugador
+					list.add(s);
+				}
+			}
+			col++;
+
+		}
+				
+		
+		
+		return list;
 	};
 
 }

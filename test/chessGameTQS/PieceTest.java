d4c0203fa4abf2734224009422ffbbde804e1d56
Alex Velasco <131alexx@gmail.com>
2019-11-16 18:08:40 +0100

    [TEST] piece & square: new tests to complete statement coverage

--- test/chessGameTQS/PieceTest.java
+++ test/chessGameTQS/PieceTest.java
@@ -1,14 +1,40 @@
 package chessGameTQS;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PieceTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEquals() {
+
+		
+		Piece p1 = new King(Board.PLAYER_1);
+		Piece p2 = new King(Board.PLAYER_2);
+		
+		assertNotEquals(p1, p2);
+		
+		p1 = new Queen(Board.PLAYER_1);
+		p2 = new Bishop(Board.PLAYER_1);
+		assertNotEquals(p1, p2);
+		
+		
+		p1 = new Queen(Board.PLAYER_1);
+		p2 = new Bishop(Board.PLAYER_2);
+		assertNotEquals(p1, p2);
+		
+		
+		p1 = new King(Board.PLAYER_1);
+		p2 = new King(Board.PLAYER_1);
+		assertEquals(p1, p2);
+		
+		
 	}
 
 }
--- test/chessGameTQS/SquareTest.java
+++ test/chessGameTQS/SquareTest.java
@@ -1,14 +1,55 @@
 package chessGameTQS;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SquareTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEquals() {
+		/* Misma pieza  mismo lugar*/
+		Square p1 = new Square(new King(Board.PLAYER_1), 0, 0);
+		Square p2 = new Square(new King(Board.PLAYER_1), 0, 0);
+		assertEquals(p1, p2);
+		
+		/* Sin pieza, mismo lugar */
+		p1 = new Square(null, 5, 5);
+		p2 = new Square(null, 5, 5);
+		assertEquals(p1, p2);
+		
+		
+		/* Sin pieza, distinto lugar*/
+		p1 = new Square(null, 5, 5);
+		p2 = new Square(null, 6, 5);
+		assertNotEquals(p1, p2);
+		
+		
+		/* Alternando con y sin pieza */
+		p1 = new Square(new King(Board.PLAYER_1), 5, 5);
+		p2 = new Square(null, 5, 5);
+		assertNotEquals(p1, p2);
+		assertNotEquals(p2, p1);
+		
+		
+		/*Misma pieza, distinto lugar */
+		p1 = new Square(new Queen(Board.PLAYER_1), 5, 5);
+		p2 = new Square(new Queen(Board.PLAYER_1), 5, 6);
+		assertNotEquals(p1, p2);
+		
+		/* Misma piexa, mismo lugar, distonto jugador */
+		p1 = new Square(new King(Board.PLAYER_1), 0, 0);
+		p2 = new Square(new King(Board.PLAYER_2), 0, 0);
+		assertNotEquals(p1, p2);
+		
+		
+		
+		
 	}
 
 }
